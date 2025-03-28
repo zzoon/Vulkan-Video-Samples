d6d062ec74936992f0fa1166a934fa5be249b41d
Hyunjun Ko <zzoon@igalia.com>
2025-03-28 12:44:30 +0900

    Fix self assignment of renderHeight when frame and render sizes are different

--- vk_video_decoder/libs/NvVideoParser/src/VulkanVP9Decoder.cpp
+++ vk_video_decoder/libs/NvVideoParser/src/VulkanVP9Decoder.cpp
@@ -514,7 +514,7 @@ void VulkanVP9Decoder::ParseFrameAndRenderSize()
         pPicData->renderHeight = u(16) + 1;
     } else {
         pPicData->renderWidth = pPicData->FrameWidth;
-        pPicData->renderHeight = pPicData->renderHeight;
+        pPicData->renderHeight = pPicData->FrameHeight;
     }
 }
 
@@ -540,7 +540,7 @@ void VulkanVP9Decoder::ParseFrameAndRenderSizeWithRefs()
                 pPicData->renderHeight = u(16) + 1;
             } else {
                 pPicData->renderWidth = pPicData->FrameWidth;
-                pPicData->renderHeight = pPicData->renderHeight;
+                pPicData->renderHeight = pPicData->FrameHeight;
             }
 
             break;
@@ -807,8 +807,8 @@ bool VulkanVP9Decoder::BeginPicture(VkParserPictureData* pnvpd)
     VkParserSequenceInfo nvsi = m_ExtSeqInfo;
     nvsi.eCodec = VK_VIDEO_CODEC_OPERATION_DECODE_VP9_BIT_KHR;
     nvsi.nChromaFormat = pPicDataVP9->ChromaFormat;
-    nvsi.nMaxWidth = width; // TODO: alignment???
-    nvsi.nMaxHeight = height;
+    //nvsi.nMaxWidth = width; // TODO: How get max width and height??
+    //nvsi.nMaxHeight = height;
     nvsi.nCodedWidth = width;
     nvsi.nCodedHeight = height;
     nvsi.nDisplayWidth = width;
@@ -846,6 +846,9 @@ bool VulkanVP9Decoder::BeginPicture(VkParserPictureData* pnvpd)
     if (m_pCurrPic == nullptr) {
         m_pClient->AllocPictureBuffer(&m_pCurrPic);
         assert(m_pCurrPic);
+
+        m_pCurrPic->decodeWidth = width;
+        m_pCurrPic->decodeHeight = height;
     }
 
     pnvpd->PicWidthInMbs = nvsi.nCodedWidth >> 4;
